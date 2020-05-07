c3e0a1978dd304d678128cb6c3f970e3f06236f2
Riya Ghosh <riyaghosh3110@gmail.com>
2020-05-07 20:53:01 +0530

    Initial commit

---
+++ app/src/main/java/com/example/automatedff/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.automatedff;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    DatabaseReference ref;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ref= FirebaseDatabase.getInstance().getReference().child("user");
+        ref.child("1").setValue("abc");
+    }
+
+}
